93ccc698f56ff29b37232411e7c60f1b1d477d6a
Tomás Lopes <tomas050302@gmail.com>
2021-05-12 18:00:50 +0100

    feat: added selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,11 @@
-sorted = []
+import random
 
 
-def get_min(arr):
-    min = arr[0]
-    min_index = 0
+def get_min(arr, start_index):
+    min = arr[start_index]
+    min_index = start_index
 
-    for i in range(len(arr)):
+    for i in range(start_index, len(arr)):
         if(arr[i] < min):
             min = arr[i]
             min_index = i
@@ -13,17 +13,35 @@ def get_min(arr):
     return min, min_index
 
 
-def selection_sort(arr, i):
+def selection_sort(arr, i=0):
     if(i == len(arr)-1):
         return arr
 
-    min, min_index = get_min(arr[i:])
+    min, min_index = get_min(arr, i)
 
     arr[i], arr[min_index] = min, arr[i]
 
     return selection_sort(arr, i+1)
 
 
-arr = [2, 4, 7, 8, 2, 3, 5, 56, 8, 9, 1, 1, 9]
+print('\n\n--- Best Case ---\n')  # O(n^2)
 
-print(selection_sort(arr, 0))
+arr = []
+for i in range(500):
+    arr.append(i)
+
+print(selection_sort(arr))
+
+print('\n\n--- Averege Case ---\n')  # O(n^2)
+arr = []
+for i in range(500):
+    arr.append(random.randint(1, 500))
+
+print(selection_sort(arr))
+
+print('\n\n--- Worst Case ---\n')  # O(n^2)
+arr = []
+for i in range(500):
+    arr.append(500-i)
+
+print(selection_sort(arr))
